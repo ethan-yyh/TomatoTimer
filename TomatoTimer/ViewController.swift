c3e456c545f81cd3152194e039723fbd7d3a178b
Yiheng Yao <yaoyiheng2014@hotmail.com>
2021-03-13 15:42:33 -0500

    Initial Commit

---
+++ TomatoTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TomatoTimer
+//
+//  Created by Yiheng Yao on 2021-03-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
